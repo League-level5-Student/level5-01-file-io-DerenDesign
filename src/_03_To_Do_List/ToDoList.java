80802a7cd30d0fe5325172be749b9d0232663c14
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2023-04-09 12:19:38 -0700

    Done

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,7 +1,13 @@
 package _03_To_Do_List;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,22 +59,36 @@ public class ToDoList implements ActionListener {
 		save = new JButton();
 		load = new JButton();
 		task = new ArrayList<String>();
+		
 		add.setText("Add tasks");
 		view.setText("View tasks");
 		remove.setText("Remove tasks");
 		save.setText("Save tasks");
 		load.setText("Load tasks");
+		
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		panel.add(remove);
+		panel.add(save);
+		panel.add(load);
+		
 		add.addActionListener(this);
 		view.addActionListener(this);
 		remove.addActionListener(this);
 		save.addActionListener(this);
 		load.addActionListener(this);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(500, 500));
+		frame.pack();
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource()==add) {
-			String text = JOptionPane.showInputDialog("");
+			String text = JOptionPane.showInputDialog("What task do you want to add?");
 			task.add(text);
 		}
 		if(e.getSource()==view) {
@@ -84,10 +104,24 @@ public class ToDoList implements ActionListener {
 			
 		}
 		if(e.getSource()==remove) {
-	
+			String tasks = JOptionPane.showInputDialog("What task would you like to remove?");
+			task.remove(tasks);
 		}
 		if(e.getSource()==save) {
-	
+			try {
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/file.txt");
+				for (String taskss : task) {
+					for (char c : taskss.toCharArray()) {
+						fw.append(c);
+					}
+					fw.append('\n');
+				}
+				
+				fw.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
 		if(e.getSource()==load) {
 			load();
@@ -95,6 +129,22 @@ public class ToDoList implements ActionListener {
 		
 	}
 	void load() {
-		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/_00_Intro_To_File_Input_and_Output/test.txt"));
+			
+			String line = br.readLine();
+			while(line != null){
+				System.out.println(line);
+				line = br.readLine();
+			}
+			
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,6 +1,8 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JFileChooser;
 
@@ -10,7 +12,7 @@ public class DirectoryIterator {
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
-		JFileChooser jfc = new JFileChooser();
+		JFileChooser jfc = new JFileChooser("C:\\Users\\Deren\\git\\level5-01-file-io-DerenDesign\\src\\_04_Directory_Iteration");
 		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		int returnVal = jfc.showOpenDialog(null);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -18,7 +20,15 @@ public class DirectoryIterator {
 			File[] files = directory.listFiles();
 			if(files != null) {
 				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
+				 if(f.getName().contains(".java")) {
+					 try {
+						 FileWriter fw = new FileWriter(f,true);
+						 System.out.println(f.getName());
+						 fw.write("//Copyright © 2022 Deren Erdem");
+					 } catch (IOException e) {
+						 e.printStackTrace();
+					 }
+				 }
 				}
 			}
 		}
